db9bbfb4fc8da8ef1b3cc202b618a3afe5f8c9d2
이현승 <gesal0303@gmail.com>
2022-07-27 20:27:59 +0900

    lhs admin 페이지 수정

--- mutsa_hackathon/accounts/admin.py
+++ mutsa_hackathon/accounts/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
-from .models import Profile
 from django.contrib.auth.models import User
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'password']
+    list_display = ['get_name', 'username', 'get_belong', 'id', 'get_number']
+
+    def get_name(self, obj):
+        return obj.profile.name
+
+    def get_belong(self, obj):
+        return obj.profile.belong
+
+    def get_number(self, obj):
+        return obj.profile.number
+
+
 admin.site.unregister(User)    
 admin.site.register(User, UserAdmin)
 
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'belong', 'number']
-    list_filter = ['belong']
-admin.site.register(Profile, ProfileAdmin)
 
--- mutsa_hackathon/whytingapp/admin.py
+++ mutsa_hackathon/whytingapp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Store,Image
+from .models import Store, Image
 
-admin.site.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address', 'number', 'waiting_state', 'id']
+
+admin.site.register(Store, StoreAdmin)
 admin.site.register(Image)
+
